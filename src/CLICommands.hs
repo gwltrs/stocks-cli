37cef78862861a7d001b30224ed8c292f9a435b9
Gregory Walters <waltersge1@gmail.com>
2020-11-26 15:49:16 -0500

    added the data-show and data-fetch-sample commands

--- src/CLICommands.hs
+++ src/CLICommands.hs
@@ -5,8 +5,9 @@ import Data.Function ((&))
 import Data.Functor ((<&>), ($>))
 import Data.Foldable (fold)
 
-import Types (CLICommand(..))
-import Prettify (prettifyCmd)
+import Types (CLICommand(..), CLIState(stocks))
+import Prettify (prettifyCmd, prettifyStocks)
+import TestStocks (testStocks)
 
 helpName :: [String]
 helpName = ["help"]
@@ -20,13 +21,12 @@ cliCommands = [
     CLICommand { 
         name = ["quit"],
         description = "Terminates the application", 
-        effect = (exitSuccess $>) }
-    -- CLICommand { 
-    --     name = ["show", "data"],
-    --     description = "Prints the state", 
-    --     effect = (\s -> (s & show & putStrLn) >> pure s) },
-    -- CLICommand { 
-    --     name = ["add"],
-    --     description = "Increments the state", 
-    --     effect = (\s -> pure (s + 1)) }
-        ]
+        effect = (exitSuccess $>) },
+    CLICommand { 
+        name = ["data", "show"],
+        description = "Prints the currently-loaded stocks data set", 
+        effect = (\s -> s & stocks & prettifyStocks & putStrLn >> pure s) },
+    CLICommand { 
+        name = ["data", "fetch", "sample"],
+        description = "Fetches the small, built-in stocks data set", 
+        effect = (\s -> putStrLn "Done" >> pure s { stocks = testStocks }) } ]
