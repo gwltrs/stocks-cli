42a4843f656a95782f102fa61eaeb6c288f65007
Gregory Walters <waltersge1@gmail.com>
2020-11-30 16:27:24 -0500

    refactored StocksCompactJSON

--- src/StocksCompactJSON.hs
+++ src/StocksCompactJSON.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module StocksCompactJSON where
+module StocksCompactJSON (toStocksCompactJSON, parseStocksCompactJSON) where
 
 import Prelude hiding (length)
 import Data.Function ((&))
@@ -8,7 +8,7 @@ import Data.Functor ((<&>))
 import Control.Category ((>>>))
 import Data.Aeson (decode, encode, Value (..))
 import Data.Text (Text, pack, unpack, intercalate)
-import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.ByteString.Lazy (ByteString, fromStrict, toStrict)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import Data.Vector (Vector, mapMaybe, toList, (!?), length)
 import Data.Scientific (toBoundedInteger, toRealFloat)
@@ -20,7 +20,7 @@ import Types (Stock(..), Day(..))
 toStocksCompactJSON :: [Stock] -> Text
 toStocksCompactJSON stocks =
     let
-        toJSONTxt str = str & pack & encode & toStrict & decodeUtf8
+        toJSONTxt = pack >>> encode >>> toStrict >>> decodeUtf8
         toJSONArray txts = 
             "[" <> (intercalate "," txts) <> "]"
         dayToTxtVals d = [
@@ -40,63 +40,77 @@ toStocksCompactJSON stocks =
 -- saved as ordered arrays instead of objects.
 parseStocksCompactJSON :: Text -> Maybe [Stock]
 parseStocksCompactJSON txt =
-    let 
-        toDay :: Value -> Maybe Day
-        toDay v = case v of
-            Array vec -> do
-                date <- (vec !? 0) >>= toString
-                open <- (vec !? 1) >>= toFloat
-                high <- (vec !? 2) >>= toFloat
-                low <- (vec !? 3) >>= toFloat
-                close <- (vec !? 4) >>= toFloat
-                volume <- (vec !? 5) >>= toInt
-                Just Day {
-                    date = date, 
-                    open = open, 
-                    high = high, 
-                    low = low, 
-                    close = close, 
-                    volume = volume }
-            _ -> Nothing
-        toStock :: Value -> Maybe Stock
-        toStock v = case v of
-            Array vec -> do
-                symbol <- (vec !? 0) >>= toString
-                days <- (vec !? 1) >>= toVector toDay 
-                Just Stock { symbol = symbol, days = toList days }
-            _ -> Nothing
-    in
-        case (txt & encodeUtf8 & fromStrict & decode :: Maybe Value) of
-            Just (Array vec) -> vec & mapMaybe toStock & toList & Just
-            _ -> Nothing
+    txt 
+        & encodeUtf8 
+        & fromStrict 
+        & (decode :: ByteString -> Maybe Value) 
+        >>= toMappedVector toStock 
+        <&> toList
 
--- Tries to parse a String from an Aeson.Value.
+-- Tries to extract a String from an Aeson.Value.
 toString :: Value -> Maybe String
 toString val = case val of
     String txt -> Just (unpack txt)
     _ -> Nothing
 
--- Tries to parse a Float from an Aeson.Value.
+-- Tries to extract a Float from an Aeson.Value.
 toFloat :: Value -> Maybe Float
 toFloat val = case val of
     Number s -> Just (toRealFloat s)
     _ -> Nothing
 
--- Tries to parse an Int from an Aeson.Value.
+-- Tries to extract an Int from an Aeson.Value.
 toInt :: Value -> Maybe Int
 toInt val = case val of
     Number s -> toBoundedInteger s
     _ -> Nothing
 
--- Tries to parse a Vector from an Aeson.Value.
+-- Tries to extract a Vector from an Aeson.Value.
+toVector :: Value -> Maybe (Vector Value)
+toVector val =
+    case val of
+        Array vec -> Just vec
+        _ -> Nothing
+
+-- Tries to extract a Vector and its elements from an Aeson.Value.
 -- Returns Nothing if at least one element fails to convert.
-toVector :: (Value -> Maybe a) -> Value -> Maybe (Vector a)
-toVector f val = case val of
+toMappedVector :: (Value -> Maybe a) -> Value -> Maybe (Vector a)
+toMappedVector f val = case val of
     Array vec -> 
-        let mappedVec = mapMaybe f vec
+        let 
+            mappedVec = mapMaybe f vec
         in
             if (length mappedVec) == (length vec) 
             then Just mappedVec
             else Nothing
     _ ->
         Nothing
+
+-- Tries to extract a Day from an Aeson.Value. 
+toDay :: Value -> Maybe Day
+toDay val = 
+    let 
+        day d o h l c v =
+            Day { date = d, open = o, high = h, low = l, close = c, volume = v }
+        dayFromVec vec = 
+            day
+                <$> (vec & (!? 0) >>= toString)
+                <*> (vec & (!? 1) >>= toFloat)
+                <*> (vec & (!? 2) >>= toFloat)
+                <*> (vec & (!? 3) >>= toFloat)
+                <*> (vec & (!? 4) >>= toFloat)
+                <*> (vec & (!? 5) >>= toInt)
+    in
+        toVector val >>= dayFromVec
+
+-- Tries to extract a Stock from an Aeson.Value.
+toStock :: Value -> Maybe Stock
+toStock val = 
+    let 
+        stock s d = Stock { symbol = s, days = toList d }
+        stockFromVec vec = 
+            stock 
+                <$> (vec & (!? 0) >>= toString)
+                <*> (vec & (!? 1) >>= toMappedVector toDay)
+    in 
+        toVector val >>= stockFromVec
