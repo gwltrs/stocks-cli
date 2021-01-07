590e34c714a14848550c19f5bcf5cd9e26f6740f
Gregory Walters <waltersge1@gmail.com>
2021-01-07 15:11:10 -0500

    moved cli command definitions out of the list literal

--- src/CLICommands.hs
+++ src/CLICommands.hs
@@ -31,63 +31,84 @@ helpName = ["help"]
 
 cliCommands :: [CLICommand]
 cliCommands = [
-    CLICommand { 
-        name = helpName,
-        description = "Prints the list of available commands", 
-        effect = cliCommands <&> prettifyCmd <&> putStrLn & fold & ($>) },
-    CLICommand { 
-        name = ["quit"],
-        description = "Terminates the application", 
-        effect = (exitSuccess $>) },
-    CLICommand { 
-        name = ["data", "show"],
-        description = "Prints the currently-loaded stocks data set", 
-        effect = (\s -> s & stocks & prettifyStocks & putStrLn >> pure s) },
-    CLICommand { 
-        name = ["data", "fetch", "sample"],
-        description = "Fetches the small, built-in stocks data set",
-        effect = (\s -> putStrLn "Done" >> pure s { stocks = validatedLiterals & testStocks }) },
-    CLICommand { 
-        name = ["data", "fetch", "eodhd"],
-        description = "Fetches a stocks data set from",
-        effect = (\s -> CIO.printAndReturn s $ do
-            apiKey <- CIO.lookupEnv eodhdAPIKeyEnvVar
-            date <- CIO.promptSymbolsDate
-            year <- CIO.promptDaysStartingYear
-            putStrLn "Fetching symbols..." & CIO.liftM
-            symbols <- CIO.getBody (symbolsURL apiKey date)
-                >>= (\body -> parseSymbols body ?? "Couldn't parse symbols")
-                -- Only keeping symbols that are entirely alphabetic and are less 
-                -- than 5 characters in length. This eliminates symbols that 
-                -- can't be traded on mainstread platforms such as Robinhood.
-                <&> V.filter (not . any (not . isAlpha))
-                <&> V.filter ((<= 4) . length)
-            putStrLn ("Got " ++ (show $ length $ symbols) ++ " symbols")
+    helpCmd,
+    quitCmd,
+    dataShowCmd,
+    dataFetchSampleCmd,
+    dataFetchEODHDCmd,
+    dataFileSaveCmd,
+    dataFileLoadCmd]
+
+helpCmd :: CLICommand
+helpCmd = CLICommand { 
+    name = helpName,
+    description = "Prints the list of available commands", 
+    effect = cliCommands <&> prettifyCmd <&> putStrLn & fold & ($>) }
+
+quitCmd :: CLICommand
+quitCmd = CLICommand { 
+    name = ["quit"],
+    description = "Terminates the application", 
+    effect = (exitSuccess $>) }
+
+dataShowCmd :: CLICommand
+dataShowCmd = CLICommand { 
+    name = ["data", "show"],
+    description = "Prints the currently-loaded stocks data set", 
+    effect = (\s -> s & stocks & prettifyStocks & putStrLn >> pure s) }
+
+dataFetchSampleCmd :: CLICommand
+dataFetchSampleCmd = CLICommand { 
+    name = ["data", "fetch", "sample"],
+    description = "Fetches the small, built-in stocks data set",
+    effect = (\s -> putStrLn "Done" >> pure s { stocks = validatedLiterals & testStocks }) }
+
+dataFetchEODHDCmd :: CLICommand
+dataFetchEODHDCmd = CLICommand { 
+    name = ["data", "fetch", "eodhd"],
+    description = "Fetches a stocks data set from",
+    effect = (\s -> CIO.printAndReturn s $ do
+        apiKey <- CIO.lookupEnv eodhdAPIKeyEnvVar
+        date <- CIO.promptSymbolsDate
+        year <- CIO.promptDaysStartingYear
+        putStrLn "Fetching symbols..." & CIO.liftM
+        symbols <- CIO.getBody (symbolsURL apiKey date)
+            >>= (\body -> parseSymbols body ?? "Couldn't parse symbols")
+            -- Only keeping symbols that are entirely alphabetic and are less 
+            -- than 5 characters in length. This eliminates symbols that 
+            -- can't be traded on mainstread platforms such as Robinhood.
+            <&> V.filter (not . any (not . isAlpha))
+            <&> V.filter ((<= 4) . length)
+        putStrLn ("Got " ++ (show $ length $ symbols) ++ " symbols")
+            & CIO.liftM
+        stocks <- CIO.getStocks apiKey year symbols
+            & CIO.liftM
+        putStrLn ("Got " ++ (show $ length $ stocks) ++ " stocks")
+            >> pure s { stocks = stocks }
+            & CIO.liftM )}
+
+dataFileSaveCmd :: CLICommand
+dataFileSaveCmd =  CLICommand {
+    name = ["data", "file", "save"],
+    description = "Saves the currently-loaded stocks data set to a file",
+    effect = (\s -> CIO.printAndReturn s $
+        if s & stocks & null
+        then CIO.returnError "No data"
+        else do
+            _ <- CIO.prompt "Path: "
                 & CIO.liftM
-            stocks <- CIO.getStocks apiKey year symbols
-                & CIO.liftM
-            putStrLn ("Got " ++ (show $ length $ stocks) ++ " stocks")
-                >> pure s { stocks = stocks }
-                & CIO.liftM )},
-    CLICommand {
-        name = ["data", "file", "save"],
-        description = "Saves the currently-loaded stocks data set to a file",
-        effect = (\s -> CIO.printAndReturn s $
-            if s & stocks & null
-            then CIO.returnError "No data"
-            else do
-                _ <- CIO.prompt "Path: "
-                    & CIO.liftM
-                    >>= (flip CIO.writeFile) (s & stocks & toStocksCompactCSV)
-                CIO.liftM (putStrLn "Done" >> pure s) ) },
-    CLICommand { 
-        name = ["data", "file", "load"],
-        description = "Loads a stocks data set from a file", 
-        effect = (\s -> CIO.printAndReturn s $ do
-            fileText <- CIO.prompt "Path: "
-                & CIO.liftM 
-                >>= CIO.readFile
-            stocks <- parseStocksCompactCSV fileText ?? "Wrong format"
-            putStrLn "Done"
-                >> pure s { stocks = stocks }
-                & CIO.liftM ) } ]
+                >>= (flip CIO.writeFile) (s & stocks & toStocksCompactCSV)
+            CIO.liftM (putStrLn "Done" >> pure s) ) }
+
+dataFileLoadCmd :: CLICommand
+dataFileLoadCmd = CLICommand { 
+    name = ["data", "file", "load"],
+    description = "Loads a stocks data set from a file", 
+    effect = (\s -> CIO.printAndReturn s $ do
+        fileText <- CIO.prompt "Path: "
+            & CIO.liftM 
+            >>= CIO.readFile
+        stocks <- parseStocksCompactCSV fileText ?? "Wrong format"
+        putStrLn "Done"
+            >> pure s { stocks = stocks }
+            & CIO.liftM ) }
